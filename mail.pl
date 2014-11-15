05aa4de6b75d5bbf1cc45835c9c1f0e3aee36058
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-15 22:47:02 +0100

    reading one email

--- mail.pl
+++ mail.pl
@@ -6,27 +6,30 @@ use HTML::Template;
 use Path::Class;
 use autodie; # die if problem reading or writing a file
 
-#print "Content-Type: text/html\n\n";
 # open the html template
 my $template = HTML::Template->new(filename => 'inbox.tmpl');
 # open mailbox
 my $file = file("/home/monika/mbox");
-# Read in the entire contents of a file
 my $content = $file->slurp();
-
-# openr() returns an IO::File object to read from
 my $file_handle = $file->openr();
 
+my $query = new CGI();
+my $startLine = $query->param('start');
+my $endLine = $query->param('end');
+
 sub start {
-	my $counter = 0;
+	print "Content-type: text/html\n\n";
+	my $counter = 1;
+	my $mail = "";
 	# Read in line at a time
 	while( my $line = $file_handle->getline() ) {
-		# we're on the first line of the next mail
+		if ($counter >= $startLine && $counter < $endLine)
+		{
+			$mail .= $line;
+		}
 		$counter++;
-		print "$counter";
-	}
-	
-    print "Content-type: text/html\n\n";
+	}	
+	print "$mail";
 }
 
 start();
