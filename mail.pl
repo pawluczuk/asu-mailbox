76944f44c326d366cea720fe5484bc96eddf4504
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-16 10:47:03 +0100

    mail reading fixes

--- mail.pl
+++ mail.pl
@@ -4,20 +4,29 @@ use warnings;
 use CGI;
 use HTML::Template;
 use Path::Class;
-use autodie;
+use autodie; # die if problem reading or writing a file
 
-print "Conent-type: text/html\n\n";
-
+#print "Content-Type: text/html\n\n";
+# open the html template
+my $template = HTML::Template->new(filename => 'inbox.tmpl');
+# open mailbox
 my $file = file("/home/monika/mbox");
+# Read in the entire contents of a file
 my $content = $file->slurp();
+
+# openr() returns an IO::File object to read from
 my $file_handle = $file->openr();
 
-my $query = new CGI();
-my $start = $query->param('start');
-my $end = $query->param('end');
-
-print "$start \t $end";
-
-while (my $line = $file_handle->getLine() ) {
-	print "$line";
+sub start {
+	my $counter = 0;
+	# Read in line at a time
+	while( my $line = $file_handle->getline() ) {
+		# we're on the first line of the next mail
+		$counter++;
+		print "$counter";
+	}
+	
+    print "Content-type: text/html\n\n";
 }
+
+start();
