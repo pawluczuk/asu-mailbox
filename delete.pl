3714efd581cc20bbe1051b950623ac58488ec28f
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-16 20:40:58 +0100

    first draft for deleting emails

---
+++ delete.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use Encode;
+use HTML::Template;
+use Path::Class;
+use autodie; # die if problem reading or writing a file
+
+# open mailbox
+my $file = file("/home/monika/mbox");
+my $content = $file->slurp();
+my $file_handle = $file->openr();
+
+my $query = new CGI();
+my $startLine = $query->param('start');
+my $endLine = $query->param('end');
+
+sub start {
+	my $counter = 1;
+	my $buffer = '';
+	# Read in line at a time
+	while( my $line = $file_handle->getline() ) {
+		if ($counter < $startLine || $counter >= $endLine)
+		{
+			$buffer .= "$line";
+		}
+		$counter++;
+	}
+	$file_handle = $file->openw();
+	$file_handle->print($buffer);
+	my $session = new CGI::Session();
+	print $session->redirect('/cgi-bin/index.pl');
+}
+
+start();
