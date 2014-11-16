640c323cbb9afffb33cf1608ef238e1295bfc094
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-16 18:47:39 +0100

    added new mail support

--- send.pl
+++ send.pl
@@ -13,49 +13,21 @@ foreach $pair (@pairs) {
 }
 
 # where is the mail program?
-$mailprog = '/usr/sbin/sendmail';
+$mailprog = '/usr/sbin/ssmtp';
 
-# change this to your own email address
-
-$recipient = 'mpawluc1@mion.elka.pw.edu.pl';
- 
-# this opens an output stream and pipes it directly to the 
-# sendmail program.  If sendmail can't be found, abort nicely 
-# by calling the dienice subroutine (see below)
+# mail details
+$from = 'mpawluc1@mion.elka.pw.edu.pl';
+$recipient = $FORM{'recipient'};
+$subject = $FORM{'subject'};
+$body = $FORM{'body'};
 
 open (MAIL, "|$mailprog -t") or dienice("Can't access 
 $mailprog!\n");
 
-# here we're printing out the header info for the mail 
-# message. You must specify who it's to, or it won't be 
-# delivered:
-
+print MAIL "From: $from\n";
 print MAIL "To: $recipient\n";
-
-# Reply-to can be set to the email address of the sender, 
-# assuming you have actually defined a field in your form
-# called 'email'.
-
-print MAIL "Reply-to: $FORM{'email'} ($FORM{'name'})\n";
-
-# print out a subject line so you know it's from your form cgi.
-# The two \n\n's end the header section of the message.  
-# anything you print after this point will be part of the 
-# body of the mail.
-
-print MAIL "Subject: Form Data\n\n";
-
-# here you're just printing out all the variables and values, 
-# just like before in the previous script, only the output 
-# is to the mail message rather than the followup HTML page.
-
-foreach $key (keys(%FORM)) {
-    print MAIL "$key = $FORM{$key}\n";
-}
-
-# when you finish writing to the mail message, be sure to 
-# close the input stream so it actually gets mailed.
-
+print MAIL "Subject: $subject\n\n";
+print MAIL "$body";
 close(MAIL);
 
 # now print something to the HTML page, usually thanking 
